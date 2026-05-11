8a830feaa1309246e9c2b7a3ea8aaffddc26101a
Ryan D Guild <ryan.d.guild@erdc.dren.mil>
2026-05-11 18:54:42 -0400

    Migrate from Ant to Gradle with Maven Central dependencies

---
+++ src/gb/tests/GitBackupTest.java
@@ -0,0 +1,12 @@
+package gb.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GitBackupTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
