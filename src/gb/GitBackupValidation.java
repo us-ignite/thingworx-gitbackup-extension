c157a971c11fd9132208fb40ca5fa11820dd48db
Ryan D Guild <ryan.d.guild@erdc.dren.mil>
2026-07-20 16:42:34 -0400

    fix: add extension migration compatibility

---
+++ src/gb/GitBackupValidation.java
@@ -0,0 +1,10 @@
+package gb;
+
+/**
+ * Compatibility class for entities created by releases before the Java package
+ * was moved to {@code gb.extension}.
+ */
+@Deprecated
+public class GitBackupValidation extends gb.extension.GitBackupValidation {
+	private static final long serialVersionUID = 9085129963750550673L;
+}
--- src/gb/extension/ExtMigrator.java
+++ src/gb/extension/ExtMigrator.java
@@ -14,10 +14,18 @@ public class ExtMigrator extends ExtensionMigratorBase {
 	private static Logger _logger = LogUtilities.getInstance().getApplicationLogger(ExtMigrator.class);
 
 	@Override
- 	public void migrate(ImportedEntityCollection imports) throws Exception {
-		_logger.warn("ExtMigrator: Starting migration.");
+	public void migrate(ImportedEntityCollection imports) throws Exception {
+		_logger.warn("ExtMigrator: Preparing migration from " + getFromVersion() + " to " + getToVersion() + ".");
+	}
+
+	@Override
+	public void postMigrate(ImportedEntityCollection imports) throws Exception {
+		_logger.warn("ExtMigrator: Starting post-import migration.");
 		// Migrated from any other version to 2.3.0
 		Thing thing = (Thing) EntityUtilities.findEntity("GIT.Utility.Thing", ThingworxRelationshipTypes.Thing);
+		if (thing == null) {
+			throw new IllegalStateException("ExtMigrator: GIT.Utility.Thing was not imported.");
+		}
 		thing.processServiceRequest("InitUserExtensionProperties", null);
 		_logger.warn("Performed one-time migration to 2.3.0.");
 
